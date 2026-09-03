a74845510459a927ea9afde427679f242a5a7830
axdityaaraj69-ctrl <axdityaaraj69@gmail.com>
2026-09-03 19:07:48 +0000

    01_basics

---
+++ js.learn/01_variable.js
@@ -0,0 +1,23 @@
+const accoundId = 123456789
+let accountEmail = "axdityaaraj@gmail.com"
+var accountPassword = "1224"
+accountCity = "Rajpur"
+let accountState;
+
+// accoountId = 2
+
+// accountId = 2 // not allowed
+
+// for (let index = 0; index < array.length; index++) {
+ //    const element = array[index];
+    
+// }
+accountEmail ="hdhcdbdjh@d.com"
+accoundPassword = "89898"
+console.log(accoundId);
+/*
+ Prefer not to use var 
+ because of issue in this block scope and functional scope
+
+*/
+console.log([accoundId,accountEmail,accountPassword,accountState]);
---
+++ js.learn/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version 
+// alert("hello")// we are using nodejs,not browser
+
+let name ="raj"
+let age = 19
+let isLoggedIn = false
+let state;
+
+// number => 2 power 53
+// bigint 
+// string => ""
+// boolean =>true/false 
+// null => standalone value 
+// undefiened => 
+// symbol => to find uniqueness
+// object
+
+console.log(typeof age)
+
