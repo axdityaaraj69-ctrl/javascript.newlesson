656ff893a8da7b14bfc348bd0de70cf8f811b523
axdityaaraj69-ctrl <axdityaaraj69@gmail.com>
2026-09-04 20:13:09 +0000

    conversionOperation.js

---
+++ js.learn/03_conversion.js
@@ -0,0 +1,29 @@
+
+// let score = "22"
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+// const {score} = req.body
+//console.log(typeof score);
+
+// "22" ==> 22
+// "33abc" ==> Nan
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1==> true; 0==> false
+// "" => false
+//  "raj" => true
+
+
+let someNumber =  33
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+console.log(typeof stringNumber);
