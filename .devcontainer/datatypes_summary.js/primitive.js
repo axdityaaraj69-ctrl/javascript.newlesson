711087a4f196f05049f9a18f7f8dd9c196b5087b
axdityaaraj69-ctrl <axdityaaraj69@gmail.com>
2026-09-05 14:05:27 +0000

    primitive

---
+++ .devcontainer/datatypes_summary.js/primitive.js
@@ -0,0 +1,37 @@
+// Primitive Datatypes
+
+
+
+//  7 types : String, Number, Boolean, Null, Undefiened, Symbol, Bigint.
+
+const score   = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+
+
+const bigNumber = 2328329828929372984928n
+
+
+
+// reference (non primitive)
+// Array, Objects, Function
+
+
+const heros = ["doga", "naagrag", "shaktiman"];
+let myobj = {
+    neme: "raj",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+    
+}
+
+console.log(typeof bigNumber);
